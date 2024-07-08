fa35d61e2b0c10182e3a34a4679409f4f5e23865
Tanvir Bhamber <77294257+BigBhamber@users.noreply.github.com>
2024-07-07 22:01:21 -0400

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(min,max) {
+    let sum = 0;
+    let temp = 0;
+    if (min>max)
+        {
+            //reverse numbers.
+            temp = max;
+            max = min;
+            min = temp;
+        }
+    if ((min<0) || (max<0))
+        {
+            return "ERROR";
+        }
+    if ( typeof min != "number" || typeof max != "number")
+        {
+            return "ERROR";
+        }
+    else 
+        for (let i = min;i<=max;i++)
+        {
+            sum+=i;
+        }
+    return sum;
 };
 
 // Do not edit below this line
