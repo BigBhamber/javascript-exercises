d4a3d7bf57ff81438b3a506f8883f39e70458177
Tanvir Bhamber <77294257+BigBhamber@users.noreply.github.com>
2024-07-07 21:42:57 -0400

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...oddItem) {
+
+    const returnArray = [];
+    for(let i =0; i< arr.length;i++)
+        {
+            if (!oddItem.includes(arr.at(i)))
+                {
+                    returnArray.push(arr.at(i));
+                }
+        }
+        return returnArray;
 };
 
 // Do not edit below this line
