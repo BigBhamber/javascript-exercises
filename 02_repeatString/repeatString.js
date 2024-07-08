498175cf72c6d26cb5138189f5e443778a5072d4
Tanvir Bhamber <77294257+BigBhamber@users.noreply.github.com>
2024-07-07 21:13:01 -0400

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num ) {
 
+    let endString="";
+    if (parseInt(num)<0)
+        {
+            return "ERROR";
+        }
+    for (let i = 0;i< parseInt(num);i++)
+        {
+            endString = endString+string;
+        }
+    return endString;
 };
 
 // Do not edit below this line
