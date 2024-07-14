483141800596a0a055e849747539b3e783051278
Tanvir Bhamber <77294257+BigBhamber@users.noreply.github.com>
2024-07-13 22:59:57 -0400

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsiusTemp = 0;
+  celsiusTemp = Math.round(((temp-32)*(5/9)*10))/10;
+  return celsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheitTemp = 0;
+  fahrenheitTemp = Math.round(((temp*(9/5))+32)*10)/10;
+  return fahrenheitTemp;
 };
 
 // Do not edit below this line
