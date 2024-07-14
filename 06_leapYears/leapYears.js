3ad664c5916bb65ee1fc0a33e93dbe0104d9672b
Tanvir Bhamber <77294257+BigBhamber@users.noreply.github.com>
2024-07-13 22:39:36 -0400

    completed exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,35 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (year%100 == 0)
+    {
+        return hundredLeapYear(year);
+    }
+    else{
+        return regularLeapYear(year);
+    }
 };
+
+function hundredLeapYear(year)
+{
+    if (year%400 == 0)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function regularLeapYear(year)
+{
+    if (year%4 == 0)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
