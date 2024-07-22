9bdccee93b030309162de7cb0e1a1073d445aaf8
Tanvir Bhamber <77294257+BigBhamber@users.noreply.github.com>
2024-07-22 18:07:42 -0400

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,37 @@
-const fibonacci = function() {
+const fibonacci = function(fibArg) {
+
+    let counter;
+    if (typeof fibArg != 'number')
+    {
+        counter = parseInt(fibArg);
+    }
+    else 
+    {
+        counter = fibArg;
+    }
+
+    if (counter<0)
+    {
+        return "OOPS";
+    }
+    else if (counter==0)
+    {
+        return 0;
+    }
+    else{
+        let firstNum = 1;
+        let secNum = 0;
+        let current;
+        for (let i = 2;i<=counter;i++)
+        {
+            current = firstNum + secNum;
+            secNum = firstNum;
+            firstNum = current;
+        }
+        return firstNum;
+    }
+
+
 
 };
 
